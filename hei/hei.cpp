48dfe3f88a1e33bf68f32e57d2f0a88174611480
joufa <areijone@gmail.com>
2016-09-03 14:31:09 +0300

    Hei

---
+++ hei/hei.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "hei.h"
+
+void otecpp_hei::hei(std::string name) {
+  std::cout << "Ohjelmoinnin tekniikka\n";
+  std::cout << name << "-kieli\n";
+  std::cout << "Verkkokurssi\n";
+}
---
+++ hei/hei.h
@@ -0,0 +1,12 @@
+#ifndef _HEI_H_
+#define _HEI_H_
+
+#include <string>
+
+namespace otecpp_hei
+{
+  void hei(std::string name);
+}
+
+#endif
+
