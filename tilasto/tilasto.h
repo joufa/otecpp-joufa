0cb4c01b7c74c0c2b1ddb8d10b4b59b9e6626dae
Antti Reijonen <areijone@gmail.com>
2016-10-05 10:58:13 +0000

    tilasto

---
+++ tilasto/tilasto.h
@@ -0,0 +1,11 @@
+#ifndef TILASTO_H
+#define TILASTO_H
+
+#include <vector>
+#include <cstdlib>
+
+namespace otecpp_tilasto
+{
+  vector<double> liukuva_ka(const vector<double> &data, unsigned int m);
+}
+#endif
